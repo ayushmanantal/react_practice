34e44eb4586f42d44a3eb0b052a942f61a1a2371
Ayushman Antal <Ayushman@redifly.com>
2021-08-17 14:21:08 +0100

    Updates

---
+++ avoid_long_lines.js
@@ -0,0 +1,13 @@
+function Button() {
+	const [counter, setCounter] = useState(0);
+  const handleClick = () => setCounter(counter+1);
+	return (<button onClick={handleClick}>
+      {counter}
+    </button>
+  );
+}
+
+ReactDOM.render(
+  <Button />,
+  document.getElementById('mountNode'),
+);
